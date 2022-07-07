f2f8459d176f82ea5271bd2655144284bb7e509f
Avatar Erick <erickbey10@gmail.com>
2022-07-07 01:40:08 -0400

    setup server

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require("dotenv");
+
+const app = express();
+
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+  );
+  
+  mongoose.connect(DB).then(() => {
+    console.log("DB connection successful");
+  });
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`App running of port ${port}`);
+  });
