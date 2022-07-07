b7f240c63b6eaa43628aceadc0ba2fbb08c32fe6
Avatar Erick <erickbey10@gmail.com>
2022-07-07 00:47:37 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import DogProfileCard from './components/DogProfileCard./DogProfileCard';
+
+function App() {
+  return (
+    <div className="App">
+      {/* login */}{/* logout */}
+
+      <section className='body'>
+        <DogProfileCard />
+      </section>
+    
+      {/* match when mutal likes */}
+    </div>
+  );
+}
+
+export default App;
