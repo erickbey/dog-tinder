8057b5b538d5dcc9074c8e0aeb742e3b04b2c52d
Erick Bey <erickbey10@gmail.com>
2022-08-01 03:49:22 -0400

    fixed connection to mongo error

--- server/app.js
+++ server/app.js
@@ -6,19 +6,21 @@ dotenv.config({ path: "../config.env" });
 
 const app = express();
 
-const DB = process.env.DB_URI.replace(
-    "<PASSWORD>",
-    process.env.DB_PASSWORD
-  );
+const MONGO_URI = process.env.DB_URI;
 
-  console.log(DB)
-  
-  mongoose.connect(DB).then(() => {
-    console.log("DB connection successful");
-  });
+if (!MONGO_URI) {
+  throw new Error('You must provide a MongoLab URI');
+}
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to Database'))
+.catch(error => console.error('Mongo Connection Error', error));
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
