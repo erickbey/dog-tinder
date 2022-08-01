7422c70686f2db7d4b1a1857637ac6803d185034
Erick Bey <erickbey10@gmail.com>
2022-08-01 17:57:24 -0400

    added app colors as variables in index file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import AccountSetup from './components/AccountSetup/AccountSetup';
-// import DogProfileCard from './components/DogProfileCard./DogProfileCard';
+// import AccountSetup from './components/AccountSetup/AccountSetup';
+import DogProfileCard from './components/DogProfileCard/DogProfileCard';
 // import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
 
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <AccountSetup />
+      {/* <AccountSetup /> */}
       {/* login */}{/* logout */}
 
-      {/* <section className='body'>
+      <section className='body'>
         <DogProfileCard />
-      </section> */}
+      </section>
     
       {/* match when mutal likes */}
     </div>
--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './NavBar.css';
+import { IoIosNotificationsOutline } from 'react-icons/io';
 
 function NavBar() {
   return (
     <div className='navbar-container'>
         <h1 className='logo-title'>Throw Me a Bone</h1>
         <div className='icons-container'>
-            <div className='profile-icon-container'></div>
+              <IoIosNotificationsOutline className='notification-icon'/>
             <div className='settings-icon-container'></div>
         </div>
     </div>
