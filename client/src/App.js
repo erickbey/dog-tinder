d5c20c9ac37b05e071ccfab15aa2e2c77e9eef15
Erick Bey <erickbey10@gmail.com>
2022-08-02 21:41:17 -0400

    changed styling for login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import DogPreviewCard from './components/DogPreviewCard/DogPreviewCard';
+// import DogPreviewCard from './components/DogPreviewCard/DogPreviewCard';
 // import AccountSetup from './components/AccountSetup/AccountSetup';
-import DogProfileCard from './components/DogProfileCard/DogProfileCard';
-// import Login from './components/Login/Login';
+// import DogProfileCard from './components/DogProfileCard/DogProfileCard';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <div className='app__container'>
         {/* <DogProfileCard /> */}
-        <DogPreviewCard />
-      </div>
+        {/* <DogPreviewCard /> */}
+        <Login />
     </div>
   );
 }
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Login.css';
+import { BiBone } from 'react-icons/bi';
 
 function Login() {
   return (
-    <div className='login-container'>
-        <h1 className='login-title'>Login</h1>
-        <p className='label'>Email</p>
-        <input className='inputs' type="text" />
-        <p className='label'>Password</p>
-        <input className='inputs' type="text" />
-        <button className='login-button'>Login</button>
+    <div className="app__container">
+      <h1 className='login__title'>Throw Me a Bone <BiBone className='icon__large bone'/></h1>
+      <div className='content__container login__container'>
+          <p className='label'>Email</p>
+          <input className='inputs' type="text" />
+          <p className='label password'>Password</p>
+          <input className='inputs' type="text" />
+          <button className='btn login__button'><span className='login__text'>Login</span></button>
+      </div>
     </div>
   )
 }
